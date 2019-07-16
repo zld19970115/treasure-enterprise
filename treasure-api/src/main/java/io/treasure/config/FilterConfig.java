cf2fd4a4e9b7b64931938340d8a96db9f6802b22
super <63600679@qq.com>
2019-07-16 03:46:47 +0000

    上传文件至 'treasure-api/src/main/java/io/treasure/config'

---
+++ treasure-api/src/main/java/io/treasure/config/FilterConfig.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2019 聚宝科技 All rights reserved.
+ *
+ * https://www.treasure.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.treasure.config;
+
+import io.treasure.common.xss.XssFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.DispatcherType;
+
+/**
+ * Filter配置
+ *
+ * @author Super 63600679@qq.com
+ */
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean xssFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setDispatcherTypes(DispatcherType.REQUEST);
+        registration.setFilter(new XssFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("xssFilter");
+        return registration;
+    }
+}
---
+++ treasure-api/src/main/java/io/treasure/config/MybatisPlusConfig.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2019 聚宝科技 All rights reserved.
+ *
+ * https://www.treasure.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.treasure.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis-plus配置
+ *
+ * @author Super 63600679@qq.com
+ * @since 1.0.0
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 配置分页
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+}
