c5e60b1dcd3edf2fb5b92f10ec33919a32581e1f
super <63600679@qq.com>
2019-07-16 03:55:48 +0000

    上传文件至 'treasure-api/src/main/java/io/treasure/service'

---
+++ treasure-api/src/main/java/io/treasure/service/TokenService.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2019 聚宝科技 All rights reserved.
+ *
+ * https://www.treasure.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.treasure.service;
+
+import io.treasure.common.service.BaseService;
+import io.treasure.entity.TokenEntity;
+
+/**
+ * 用户Token
+ * 
+ * @author Super 63600679@qq.com
+ */
+public interface TokenService extends BaseService<TokenEntity> {
+
+	TokenEntity getByToken(String token);
+
+	/**
+	 * 生成token
+	 * @param userId  用户ID
+	 * @return        返回token信息
+	 */
+	TokenEntity createToken(Long userId);
+
+	/**
+	 * 设置token过期
+	 * @param userId 用户ID
+	 */
+	void expireToken(Long userId);
+
+}
---
+++ treasure-api/src/main/java/io/treasure/service/UserService.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2019 聚宝科技 All rights reserved.
+ *
+ * https://www.treasure.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.treasure.service;
+
+import io.treasure.common.service.BaseService;
+import io.treasure.entity.UserEntity;
+import io.treasure.dto.LoginDTO;
+
+import java.util.Map;
+
+/**
+ * 用户
+ * 
+ * @author Super 63600679@qq.com
+ */
+public interface UserService extends BaseService<UserEntity> {
+
+	UserEntity getByMobile(String mobile);
+
+	UserEntity getUserByUserId(Long userId);
+
+	/**
+	 * 用户登录
+	 * @param dto    登录表单
+	 * @return        返回登录信息
+	 */
+	Map<String, Object> login(LoginDTO dto);
+}
