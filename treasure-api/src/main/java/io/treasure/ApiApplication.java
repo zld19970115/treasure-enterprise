d349eec05996c3e22816fe7829361db1da9fa9e0
super <63600679@qq.com>
2019-07-16 03:43:25 +0000

    上传文件至 'treasure-api/src/main/java/io/treasure'

---
+++ treasure-api/src/main/java/io/treasure/ApiApplication.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2019 聚宝科技 All rights reserved.
+ *
+ * https://www.treasure.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.treasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * treasure-api
+ *
+ * @author Super 63600679@qq.com
+ */
+@SpringBootApplication
+public class ApiApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiApplication.class);
+	}
+}
