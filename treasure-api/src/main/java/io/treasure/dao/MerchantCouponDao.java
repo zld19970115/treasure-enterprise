a65b3d8e0da3cb4bb44affb8685f4b360b55ebbf
zhangguanglin <494535675@qq.com>
2019-09-17 18:29:30 +0800

    告知用户还差多少可使用满减
    Signed-off-by: zhangguanglin <494535675@qq.com>

--- treasure-api/src/main/java/io/treasure/dao/MerchantCouponDao.java
+++ treasure-api/src/main/java/io/treasure/dao/MerchantCouponDao.java
@@ -1,8 +1,11 @@
 package io.treasure.dao;
 
 import io.treasure.common.dao.BaseDao;
+import io.treasure.dto.MerchantCouponDTO;
 import io.treasure.entity.MerchantCouponEntity;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * 商户端优惠卷
@@ -15,4 +18,6 @@ public interface MerchantCouponDao extends BaseDao<MerchantCouponEntity> {
     void updateStatusById(long id,int status);
 
     MerchantCouponEntity getAllById(Long id);
+
+    List<MerchantCouponDTO> getMoneyOffByMerchantId(long merchantId,long userId);
 }
