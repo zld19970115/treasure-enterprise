467c0bae9b9aecc3b56fb0923d2e95d2e323d0ed
zhangguanglin <494535675@qq.com>
2019-10-24 11:00:01 +0800

    全部订单和相关联订单 添加商品详细信息 Signed-off-by: zhangguanglin <494535675@qq.com>

--- treasure-api/src/main/java/io/treasure/dao/GoodDao.java
+++ treasure-api/src/main/java/io/treasure/dao/GoodDao.java
@@ -25,4 +25,6 @@ public interface GoodDao extends BaseDao<GoodEntity> {
     void updateStatusById(@Param("id") long id,@Param("status")int status);
     //根据id查询
     GoodDTO getByInfo(long id);
+
+    GoodEntity getByid(long id);
 }
