215e54ad9cb50fca79aa03f9c01c97645e4ac1f8
zhangguanglin <494535675@qq.com>
2019-10-24 09:42:27 +0800

    解决赠送件sql报错
    Signed-off-by: zhangguanglin <494535675@qq.com>

--- treasure-api/src/main/java/io/treasure/dao/RecordGiftDao.java
+++ treasure-api/src/main/java/io/treasure/dao/RecordGiftDao.java
@@ -3,12 +3,13 @@ package io.treasure.dao;
 import io.treasure.common.dao.BaseDao;
 import io.treasure.entity.RecordGiftEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Mapper
 public interface RecordGiftDao extends BaseDao<RecordGiftEntity> {
-   void insertRecordGift(long userId, Date date, BigDecimal balanceGift, BigDecimal useGift);
-   void insertRecordGift2(long userId, Date date, BigDecimal balanceGift, BigDecimal useGift);
+   void insertRecordGift(@Param("userId") long userId,@Param("date") Date date,@Param("balanceGift") BigDecimal balanceGift,@Param("useGift") BigDecimal useGift);
+   void insertRecordGift2(@Param("userId") long userId,@Param("date") Date date,@Param("balanceGift") BigDecimal balanceGift,@Param("useGift") BigDecimal useGift);
 }
