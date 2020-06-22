8bbbe404dfee1ce8cd81cb56844308d7b42fe893
gesiyu <784564694@qq.com>
2020-06-22 21:33:17 +0800

    gesiyu

--- treasure-api/src/main/java/io/treasure/controller/ApiAppVersionController.java
+++ treasure-api/src/main/java/io/treasure/controller/ApiAppVersionController.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.util.Date;
 import java.util.Map;
 
 
@@ -95,11 +96,52 @@ public class ApiAppVersionController {
     @Login
     @DeleteMapping
     @ApiOperation("删除")
-    
+
     public Result delete(@RequestBody Long[] ids){
         //效验数据
         AssertUtils.isArrayEmpty(ids, "id");
         appVersionService.delete(ids);
         return new Result();
+    }
+
+    @GetMapping("pageList")
+    @ApiOperation("分页查询PC")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = Constant.PAGE, value = "当前页码，从1开始", paramType = "query", required = true, dataType="int") ,
+            @ApiImplicitParam(name = Constant.LIMIT, value = "每页显示记录数", paramType = "query",required = true, dataType="int") ,
+            @ApiImplicitParam(name = "startDate", value = "开始time", paramType = "query",dataType="String") ,
+            @ApiImplicitParam(name = "endDate", value = "结束time", paramType = "query",dataType="String")
+    })
+    public Result<PageData<AppVersionDTO>> pageList(@ApiIgnore @RequestParam Map<String, Object> params) {
+        return new Result<PageData<AppVersionDTO>>().ok(appVersionService.pageList(params));
+    }
+
+    @GetMapping("delPC")
+    @ApiOperation("删除PC")
+    public Result delPC(@RequestParam Long id){
+        appVersionService.deleteById(id);
+        return new Result().ok("ok");
+    }
+
+    @GetMapping("selectById")
+    @ApiOperation("查询详情")
+    public Result selectById(@RequestParam Long id){
+        return new Result().ok(appVersionService.selectById(id));
+    }
+
+    @PostMapping("insertPC")
+    @ApiOperation("保存PC")
+    public Result insertPC(@RequestBody AppVersionDTO dto){
+        dto.setCreateDate(new Date());
+        appVersionService.save(dto);
+        return new Result().ok("ok");
+    }
+
+    @PostMapping("updatePC")
+    @ApiOperation("更新PC")
+    public Result updatePC(@RequestBody AppVersionDTO dto){
+        dto.setUpdateDate(new Date());
+        appVersionService.update(dto);
+        return new Result().ok("ok");
     }
 }
