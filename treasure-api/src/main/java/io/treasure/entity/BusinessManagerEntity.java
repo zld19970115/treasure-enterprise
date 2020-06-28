ae42dbd2f887a7f3c97eb841f549171ae437cd74
zld19970115 <249766021@qq.com>
2020-06-28 13:51:26 +0800

    更新代码自GIT

--- treasure-api/src/main/java/io/treasure/entity/BusinessManagerEntity.java
+++ treasure-api/src/main/java/io/treasure/entity/BusinessManagerEntity.java
@@ -2,6 +2,7 @@ package io.treasure.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.*;
 
@@ -16,19 +17,23 @@ import java.util.Date;
 public class BusinessManagerEntity {
 
     @TableId(value = "id",type = IdType.INPUT)
-    private Integer id;
+    private Long id;
     private String mobile;
-    private String nick_name;   //char(20)                            null,
+    private String nickName;   //char(20)                            null,
     private String password;    //char(128)                           not null,
-    private Date contract_expire;   //datetime                            null,
+    private Date contractExpire;   //datetime                            null,
     private String icon;        //char(200)                           null comment '头像',
     private String position;    //char(10)                            null comment '职位',
-    private String real_name;   //char(10)                            not null,
+    private String realName;   //char(10)                            not null,
     private String uid;         //char(20)                            not null comment '身份证号',
     private Date hiredate;      //timestamp default CURRENT_TIMESTAMP null comment '入职时间',
     private Integer star;       //int(10)   default 10000             null comment '评级',
-    private String emergent_contact;//char(11)                            null comment '紧急联系人',
-    private Integer wage_base;  //int(10)   default 0                 null comment '工资基数：单位分',
+    private String emergentContact;//char(11)                            null comment '紧急联系人',
+    private Integer wageBase;  //int(10)   default 0                 null comment '工资基数：单位分',
     private Integer status;     //int(2)                              null comment '状态1:试用，2：正式入职，3:离
-
+    /**
+     * 逻辑删除字段
+     */
+    @TableLogic
+    private Integer deleted;
 }
