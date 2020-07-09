6f5f3cab33f72d64ebd526d7775fb10c74616133
wangjinge <1397298841@qq.com>
2020-07-09 08:28:13 +0800

    unipush

--- treasure-api/src/main/java/io/treasure/push/AppPushUtil.java
+++ treasure-api/src/main/java/io/treasure/push/AppPushUtil.java
@@ -30,7 +30,49 @@ public class AppPushUtil {
         //String clientId="bed827bcb12f99ebb004180ee0cfa73d";
         //String clientId="42a6ceff19d73a608bc2cbf61ed0d86b";
         String clientId="eb55b5961ab55896101ec7137c2d7fbd";
-        pushToSingleMer("订单管理","已经接单",clientId);
+        pushToSingleMerchant("订单管理","已经接单",clientId);
+    }
+
+
+    /**
+     * 客户端对单个用户推送消息
+     *
+     * 场景1：某用户发生了一笔交易，银行及时下发一条推送消息给该用户。
+     *
+     * 场景2：用户定制了某本书的预订更新，当本书有更新时，需要向该用户及时下发一条更新提醒信息。
+     * 这些需要向指定某个用户推送消息的场景，即需要使用对单个用户推送消息的接口。
+     * title 标题
+     * text 内容
+     * logo 图标
+     * masterSect
+     * clientId
+     */
+    public  static void pushToSingleClient(String title,String text,String logo,String clientId) {
+        IGtPush push = new IGtPush(AppInfo.APPKEY_CLIENT, AppInfo.MASTERSECRET_CLIENT);
+        AbstractTemplate template = PushTemplate.getNotificationTemplate(AppInfo.APPID_CLIENT,AppInfo.APPKEY_CLIENT,title,text,logo); //通知模板(点击后续行为: 支持打开应用、发送透传内容、打开应用同时接收到透传 这三种行为)
+//        AbstractTemplate template = PushTemplate.getLinkTemplate(); //点击通知打开(第三方)网页模板
+//        AbstractTemplate template = PushTemplate.getTransmissionTemplate(); //透传消息模版
+//        AbstractTemplate template = PushTemplate.getRevokeTemplate(); //消息撤回模版
+//        AbstractTemplate template = PushTemplate.getStartActivityTemplate(); //点击通知, 打开（自身）应用内任意页面
+
+        // 单推消息类型
+        SingleMessage message = getSingleMessage(template);
+        Target target = new Target();
+        target.setAppId(AppInfo.APPID_CLIENT);
+        target.setClientId(clientId);
+//        target.setAlias(ALIAS); //别名需要提前绑定
+        IPushResult ret = null;
+        try {
+            ret = push.pushMessageToSingle(message, target);
+        } catch (RequestException e) {
+            e.printStackTrace();
+            ret = push.pushMessageToSingle(message, target, e.getRequestId());
+        }
+        if (ret != null) {
+            System.out.println(ret.getResponse().toString());
+        } else {
+            System.out.println("服务器响应异常");
+        }
     }
 
     private static APNPayload getAPNPayload(String text) {
