85ef126404296023469ce436a6c91153dcef79b9
zld19970115 <249766021@qq.com>
2020-07-27 10:34:57 +0800

    更新代码自GIT

--- treasure-api/src/main/java/io/treasure/controller/DistributionRewardController.java
+++ treasure-api/src/main/java/io/treasure/controller/DistributionRewardController.java
@@ -6,12 +6,18 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.treasure.common.constant.Constant;
 import io.treasure.common.utils.Result;
+import io.treasure.dao.DistributionRewardLogDao;
+import io.treasure.entity.ClientUserEntity;
+import io.treasure.entity.DistributionRewardLogEntity;
+import io.treasure.service.ClientUserService;
 import io.treasure.service.ClientUserVersionService;
 import io.treasure.service.impl.DistributionRewardServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/distribution_reward")
@@ -20,7 +26,10 @@ public class DistributionRewardController {
 
     @Autowired
     private DistributionRewardServiceImpl distributionRewardService;
-
+    @Autowired
+    private DistributionRewardLogDao distributionRewardLogDao;
+    @Autowired
+    private ClientUserService clientUserService;
     @GetMapping("/reward")
     @ApiOperation("分销奖励")
     @ApiImplicitParams({
@@ -35,5 +44,19 @@ public class DistributionRewardController {
 
         return new Result().ok("success");
     }
+    @GetMapping("/rewardList")
+    @ApiOperation("分销奖励列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "用户id", paramType = "query", required = true, dataType="Long")
+    })
+    public Result rewardList(Long userId) {
 
+        ClientUserEntity clientUserEntity = clientUserService.selectById(userId);
+        if (clientUserEntity==null){
+            return new Result().error("没有该用户，请稍后再试");
+        }
+        List<DistributionRewardLogEntity> distributionRewardLogEntities = distributionRewardLogDao.selectByMaterMobile(clientUserEntity.getMobile());
+
+        return new Result().ok(distributionRewardLogEntities);
+    }
 }
