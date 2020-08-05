5d65d1d08a653b8c5a7adf7c16f22371081e926c
Administrator <infinite2022@163.com>
2020-08-05 16:15:16 +0800

    modify_base_params

--- treasure-api/src/main/java/io/treasure/utils/MyPingyInUtil.java
+++ treasure-api/src/main/java/io/treasure/utils/MyPingyInUtil.java
@@ -5,20 +5,27 @@ import org.junit.Test;
 
 public class MyPingyInUtil {
 
+    @Test
+    public void test(){
+        for(int i=15878;i<19968;i++){
+            System.out.print((char)i+"="+toPyViaChar((char)i,false));
+        }
+
+    }
 
     //转成拼音
     static String toPyViaChar(char chineseChar,boolean firstWordOnly){
 
         int charCode = (int)chineseChar;
+        if(charCode == 15878){
+            if(firstWordOnly)
+                return "k";
+            return "kao";
+        }
         if(charCode>=19968 && charCode<=40869){
 
             String[] strings = PinyinHelper.toHanyuPinyinStringArray(chineseChar);
 
-            if(charCode == 15878){
-                if(firstWordOnly)
-                    return "k";
-                return "kao";
-            }
             if(strings != null && strings.length >0){
                 String tmp = strings[0];
                 if(tmp.length()>1){
