f8a4bdf8f244d1c6891f3324b1809962bc109286
Administrator <infinite2022@163.com>
2020-09-09 10:26:40 +0800

    modify_base_params

--- treasure-api/src/main/java/io/treasure/entity/SignedRewardSpecifyTimeEntity.java
+++ treasure-api/src/main/java/io/treasure/entity/SignedRewardSpecifyTimeEntity.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class SignedRewardSpecifyTimeEntity {
 
     @TableId
-    private Long id;                        //
+    private Long id;                       //
     private Integer rewardType;            // int null comment '1宝币',
     private Integer rewardValue;           // int null comment '奖励值',
     private Integer personAmount;          // int null comment '人员数量',
@@ -20,6 +20,19 @@ public class SignedRewardSpecifyTimeEntity {
     private Integer times;
     private Integer maxValue;
     private Integer minValue;
-    private Integer activityMode;            //1数量模式，2仅范围模式
+    private Integer activityMode;           //1数量模式，2仅范围模式
+
+    private Integer timesInTimeRange;       //
+    private Long couponActivityId;          //
+
+    /*
+    alter table signed_reward_specify_time modify times int null comment '单次活动的次数限制';
+
+    alter table signed_reward_specify_time
+        add times_in_time_range int null comment '多长时间才可以抢一次' after times;
+
+    alter table signed_reward_specify_time
+        add coupon_activity_id bigint null comment '活动id' after times_in_time_range;
+     */
 
 }
